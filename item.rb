3ea641258c5c6fb70da9c229428807da3e5398ae
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-15 20:12:56 +0200

    Update the item class

--- item.rb
+++ item.rb
@@ -1,13 +1,27 @@
 require 'date'
 
 class Item
-  attr_accessor :author, :label, :genre
-  attr_reader :publish_date, :archived, :id
+  attr_reader :publish_date, :archived, :id, :author, :label, :genre
 
-  def initialize(id, publish_date, archived: false)
+  def initialize(publish_date, id = nil, archived: false)
     @publish_date = Date.parse(publish_date)
     @archived = archived
     @id = id || Random.rand(1..1000)
+  end
+
+  def genre=(genre)
+    @genre = genre.name
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = { first_name: author.first_name, last_name: author.last_name }
+    author.items << self unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label.name
+    label.items << self unless label.items.include?(self)
   end
 
   def can_be_archived?
